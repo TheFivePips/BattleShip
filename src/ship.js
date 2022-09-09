610fd8fddbabe66526ff6b357b3a96a66bd4d387
sam gardner <samgardner20@gmail.com>
2022-09-09 12:50:25 -0400

    initial commit. Setting up enviornment and made first ship factory. no testing yet

---
+++ src/ship.js
@@ -0,0 +1,32 @@
+
+// each ship needs a length, where the have been hit, and if they have been sunk
+
+class Ship {
+    constructor(name, length){
+        this.name = name
+        this.length = length
+        this.body = [...Array(length)].map(x => 'O');
+        this.sunk = false
+
+    }
+    
+    getName() {return this.name}
+    
+    getBody() {return this.body}
+    
+   isSunk(){
+        this.body.every((item) => item === "X") ? console.log(`You sunk my ${this.name}`) : console.log("Not sunk yet");
+        
+    }
+    
+    hit(position) {
+        this.body[position] = "X"
+        console.log("Hit!");
+        this.isSunk()
+    }
+} 
+
+const patrolBoat = new Ship("Patrol Boat", 2)
+
+
+
