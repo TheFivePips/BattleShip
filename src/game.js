eed5aec0c8b51d55fa5910e907093725845a3615
sam gardner <samgardner20@gmail.com>
2022-09-13 10:33:34 -0400

    added files for man game loop and dom manipulation

---
+++ src/game.js
@@ -0,0 +1,20 @@
+import { Player } from "./player";
+
+let p1
+let p2
+
+function initGame() {
+    p1 = new Player("You")
+    p2 = new Player("Your Enemy")
+
+    // set turn to p1
+    p1.isTurn(p2)
+    // computer needs a random fleet
+    p2.randomFleet()
+
+    // need to make some dom rendering methods that will go here
+    // this should render a start button and a random button
+    renderButtons(p1)
+    // this should render the player boards
+    renderBoards(p1,p2)
+}
